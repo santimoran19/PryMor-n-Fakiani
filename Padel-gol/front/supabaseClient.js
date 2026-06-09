8a350d03b0601b33a44cc632addbae808aae66de
santimoran19 <santimorann19@gmail.com>
2026-06-08 21:31:16 -0300

    feat(front): set real Supabase anon key in supabaseClient.js

--- Padel-gol/front/supabaseClient.js
+++ Padel-gol/front/supabaseClient.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://gbmnqxynivnlbbsvchsu.supabase.co'
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdibnFxeXluaXZubGJic3ZjaHN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMjc2NjUsImV4cCI6MjA5NTkwMzY2NX0.JzSI7omMtiJfLDlnGW3d4QJoAd2WvL7a_NGx1fRq9T8'
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 window.supabaseClient = supabaseClient
